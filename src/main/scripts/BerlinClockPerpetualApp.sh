16ffe9d106278b43525f67ac610809b3c6030ef0
jonesal26gc <holly001git>
2017-03-08 14:35:58 +0000

    Add BASH script and run on Mac OS.

---
+++ src/main/scripts/BerlinClockPerpetualApp.sh
@@ -0,0 +1,3 @@
+# this file will execute the BerlinClock.
+cd ~
+nohup java -cp /Users/tonyjones/ProjectsGit/BerlinClock/target/BerlinClock-2.2-SNAPSHOT.jar BerlinClock.BerlinClockPerpetualApp "07" "18" "10" "1000" 1> ~/BerlinClock.log &
